e68f9c3bfb3fdfa917d2521d4907fc57d23ecc8d
andyxzdev <andrewsgamer123@outlook.com>
2026-02-06 13:59:13 -0300

    att1

--- front-end/barreto_e_santana_adv/src/App.jsx
+++ front-end/barreto_e_santana_adv/src/App.jsx
@@ -1,35 +1,259 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./App.css";
+
+import IconPhone from "./assets/icons/phone.svg";
+import IconMail from "./assets/icons/mail.svg";
+import IconeDirCivil from "./assets/icons/civil.svg";
+import LogoPequena from "./assets/icons/logo.png";
+import Arrow from "./assets/icons/arrow.svg";
+
+import ImgDra1 from "./assets/arts/art1.png";
+import ImgDra2 from "./assets/arts/art2.png";
+import ImgAreas from "./assets/arts/art_areas_atuacao.png";
+
+import Header from "../src/assets/components/header/Header.jsx";
+import "../src/assets/components/header/Header.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+      offset: 120,
+      easing: "ease-in-cubic",
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="Contatos_topo">
+          <div className="infos">
+            <div className="contato1">
+              <img src={IconPhone} alt="" />
+              <p>(99) 99999-9999</p>
+            </div>
+            <div className="contato2">
+              <img src={IconMail} alt="" />
+              <p>contato@barretoesantana.com.br</p>
+            </div>
+          </div>
+        </div>
+        <Header />
+        <section className="Hero">
+          <div className="infos">
+            <h1>
+              Defendendo seus direitos com ética, estratégia e compromisso.
+            </h1>
+            <h3>
+              Atuamos com seriedade e proximidade em todo o Brasil, oferecendo
+              soluções jurídicas claras, humanas e eficazes para cada cliente.
+            </h3>
+            <button className="Button_hero">Conheça a Barreto & Santana</button>
+          </div>
+        </section>
+
+        <section className="Muito_alem">
+          <div className="infos">
+            <img src={LogoPequena} alt="" className="Logo_pequena" />
+            <h1>
+              Muito além do Direito.<br></br>Um compromisso com pessoas.
+            </h1>
+            <h3>
+              Nossa atuação é guiada por ética, transparência e
+              responsabilidade. Acreditamos em um atendimento próximo,
+              estratégico e personalizado, onde cada caso é tratado com atenção
+              real e respeito à história de cada cliente.
+            </h3>
+            <img src={Arrow} alt="" className="Arrow" />
+          </div>
+        </section>
+
+        {/* SECTION SOBRE */}
+
+        <section className="Sobre">
+          <div className="Titulo">
+            <h1>
+              Quem está por trás<br></br>da Barreto & Santana
+            </h1>
+          </div>
+
+          <div className="infos">
+            <div className="adv1">
+              <div className="texto">
+                <h1>Dra. Anna Clara Barreto</h1>
+                <li>
+                  <h3>
+                    Advogada com atuação sólida nas áreas de Direito Civil,
+                    Família e Consumidor.
+                  </h3>
+                </li>
+                <li>
+                  <h3>
+                    Reconhecida pela postura estratégica, atendimento humanizado
+                    e dedicação em buscar as melhores soluções jurídicas para
+                    seus clientes.
+                  </h3>
+                </li>
+              </div>
+              <img src={ImgDra1} alt="" className="Img_dra1" />
+            </div>
+            <div className="adv2">
+              <img src={ImgDra2} alt="" className="Img_dra2" />
+
+              <div className="texto">
+                <h1>Dra. Anna Clara Barreto</h1>
+                <li>
+                  <h3>
+                    Advogada com atuação sólida nas áreas de Direito Civil,
+                    Família e Consumidor.
+                  </h3>
+                </li>
+                <li>
+                  <h3>
+                    Reconhecida pela postura estratégica, atendimento humanizado
+                    e dedicação em buscar as melhores soluções jurídicas para
+                    seus clientes.
+                  </h3>
+                </li>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="Resultados">
+          {/* <Resultados></Resultados> */}
+        </section>
+
+        <section className="Areas">
+          <div className="infos">
+            <img src={ImgAreas} alt="" />
+            <div className="textos">
+              <div className="titulo">
+                <h1>Áreas de Atuação</h1>
+                <h3>
+                  Atuamos de forma estratégica nas principais áreas do Direito,
+                  <br></br>
+                  sempre com foco em soluções claras e eficientes.
+                </h3>
+              </div>
+
+              <div className="textos_areas">
+                <div className="coluna">
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                </div>
+                <div className="coluna">
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                  <div className="area1">
+                    <img src={IconeDirCivil} alt="" />
+                    <h1>Direito Civil</h1>
+                    <li>
+                      <p>
+                        Atua na resolução de conflitos do dia a dia entre
+                        pessoas e empresas, como contratos, cobranças,
+                        indenizações e responsabilidades.
+                      </p>
+                    </li>
+                    <li>
+                      <p>
+                        Tem como objetivo garantir direitos e deveres nas
+                        relações civis.
+                      </p>
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ front-end/barreto_e_santana_adv/src/assets/components/header/Header.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./Header.css";
+import Logo from "../../logo/Logo2.png";
+
+const Header = ({ customStyle }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <header>
+      <div className="Logo" style={customStyle}>
+        <img src={Logo} alt="logoTopo" />
+      </div>
+
+      <button className="menu_toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <nav className={`nav ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#valores" onClick={toggleMenu}>
+              Valores
+            </a>
+          </li>
+          <li>
+            <a href="#sobre_nos" onClick={toggleMenu}>
+              Sobre nós
+            </a>
+          </li>
+          <li>
+            <a href="#Resultados" onClick={toggleMenu}>
+              Resultados
+            </a>
+          </li>
+          <li>
+            <a href="#Áreas de atuação" onClick={toggleMenu}>
+              Áreas de atuação
+            </a>
+          </li>
+          <li>
+            <a href="#Feedbacks" onClick={toggleMenu}>
+              Feedbacks
+            </a>
+          </li>
+          <li>
+            <a href="#Como_ajudamos" onClick={toggleMenu}>
+              Como ajudamos
+            </a>
+          </li>
+          <button
+            className="ButtonTopo"
+            onClick={() => window.open("https://wa.me/5524993215864", "_blank")}
+          >
+            Contato
+          </button>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
